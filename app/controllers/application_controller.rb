b0f65a246d5c17ce803abfce3b34c0422c25dd3e
ChristinaAbi <clc731e.com>
2020-10-22 18:03:31 -0400

    Authentication and Authorization complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,4 +14,12 @@ class ApplicationController < ActionController::API
     rescue
         render json: { status: 401, message: 'Unauthorized' }
     end
+    def get_current_user
+        return if !bearer_token
+        decoded_jwt = decode_token(bearer_token)
+        User.find(decoded_jwt[0]["user"]["id"])
+    end
+    def authorize_user
+        render json: { status: 401, message: "Unauthorized"} unless get_current_user.id == params[:id].to_i
+    end    
 end
